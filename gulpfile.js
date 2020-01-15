fcb559315d721bfa1c53f7971fa8b635da71cf64
Evan Rusmisel <evanrusmisel@gmail.com>
2020-01-14 18:58:05 -0600

    Update gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 const	{ src, dest, watch, task, series, parallel } = require('gulp'),
+	{ execFile } = require('child_process'),
+	hugoBin = require('hugo-bin'),
 	postcss = require('gulp-postcss'),
 	cssimport = require('postcss-import'),
 	postcssEnv = require('postcss-preset-env'),
 	cssnano = require('cssnano'),
 	babel = require('gulp-babel'),
-	del = require('del');
+	del = require('del'),
+	browserSync = require('browser-sync').create();
 
 function css() {
 	return src('src/**/*.css')
@@ -18,13 +21,28 @@ function js() {
 		.pipe(dest('assets'));
 }
 
-function clean() {
-	return del(['public', 'resources', 'assets/*']);
+function hugo(cb) {
+	execFile(hugoBin, ['-b', '/'], { cwd: 'exampleSite' }, (e, o) => {
+		console.log(o);
+		cb(e)
+	});
 }
 
-const build = parallel(css, js);
+function clean() {
+	return del(['exampleSite/public', 'exampleSite/resources', 'assets/*']);
+}
+
+const	buildAssets = parallel(css, js),
+	build = series(buildAssets, hugo);
 const watcher = () => {
-	return watch([ 'src' ], build);
+	browserSync.init({
+		watch: true,
+		server: 'exampleSite/public',
+		notify: false,
+		ghostMode: false
+	});
+	watch([ 'src' ], buildAssets)
+	watch([ 'archetypes', 'assets', 'i18n', 'layouts', 'theme.toml', 'exampleSite/content', 'exampleSite/static', 'exampleSite/config.toml' ], hugo)
 };
 
 task('build', series(clean, build));
